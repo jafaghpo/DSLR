24874eb48a07c43fcd7a86ef1a2f7107a4bd2014
John AFAGHPOUR <jafaghpo@e2r9p18.42.fr>
2018-05-10 21:56:22 +0200

    [ADD] organization of data

--- describe.py
+++ describe.py
@@ -5,15 +5,57 @@ __author__ = 'John Afaghpour'
 
 import sys
 from pandas import read_csv
+import csv
+
+
+def organize_data(file, table=[]):
+	
+	column = {}
+	first = False
+
+	for row in file:
+		if not first:
+			fields = row
+			first = True
+			continue
+		for i, elem in enumerate(row):
+			column[fields[i]] = elem
+		table.append(column.copy())
+	return table, fields
+
+
+def valid(column):
+
+	for elem in column:
+		try:
+			pass
+
+
+def parse_table(table, fields, stats=[]):
+
+	for category in fields:
+		if valid(table[category])
+			pass
 
 
 def main():
 
 	if len(sys.argv) != 2:
-		print 'usage: ' + __file__ + ' file.csv'
+		print('usage: {} file.csv'.format(__file__))
 		sys.exit()
-	data = read_csv(sys.argv[1])
-	print data
+
+	try:
+		fd = open(sys.argv[1], 'r')
+	except:
+	 	print('error: failed to open {}'.format(sys.argv[1]))
+	 	sys.exit()
+
+	file = csv.reader(fd, delimiter=',')
+	table, fields = organize_data(file)
+	for elem in table:
+		print elem
+		break
+	fd.close()
 
 if __name__ == '__main__':
 	main()
